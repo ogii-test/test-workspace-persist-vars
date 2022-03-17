88c8b70366e96a9dbdc3b4d294d40534e0ce5f58
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 16:30:11 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -6,7 +6,7 @@ API_URL="gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM"
 sleep 1s
 
 BUILD_INFO=$(curl -X GET -H "Circle-Token: $CIRCLECI_TOKEN" -H "Content-type: application/json" "https://circleci.com/api/v1.1/project/${API_URL}")
-FAILED_STEP=$(echo $BUILD_INFO | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')
+FAILED_STEP=\"$(echo $BUILD_INFO | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')\"
 STEP=$(echo "${FAILED_STEP}" | jq -r ".step")
 echo "export STEP=${STEP}" | sudo tee -a $BASH_ENV
 NAME=$(echo "${FAILED_STEP}" | jq -r ".name")
