bffcc23e0904a1843b8a9c9456ca63d83ebee3be
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 13:36:16 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -3,12 +3,12 @@
 
 API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)
 FAILED_STEP=$(curl "https://circleci.com/api/v1.1/project/${API_URL}?circle-token=${CIRCLE_API_TOKEN}" | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')
-echo 'export THE_FAILED_STEP=$FAILED_STEP' | sudo tee -a $BASH_ENV
-echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' | sudo tee -a $BASH_ENV
-echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' | sudo tee -a $BASH_ENV
-echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' | sudo tee -a $BASH_ENV
-echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' | sudo tee -a $BASH_ENV
-echo 'export COMMITTER_NAME=$( curl "https://circleci.com/api/v1.1/project/${API_URL}?circle-token=${CIRCLE_API_TOKEN}" | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' | sudo tee -a $BASH_ENV
+echo 'export THE_FAILED_STEP=$FAILED_STEP' >> $BASH_ENV
+echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' >> $BASH_ENV
+echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
+echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' >> $BASH_ENV
+echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' >> $BASH_ENV
+echo 'export COMMITTER_NAME=$( curl "https://circleci.com/api/v1.1/project/${API_URL}?circle-token=${CIRCLE_API_TOKEN}" | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' >> $BASH_ENV
 echo $COMMITTER_NAME >> envars.txt
 echo $CIRCLE_API_TOKEN  >> envars.txt
 echo $ALLOCATION_ID  >> envars.txt
