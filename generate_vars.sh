3b29c625715a8893d1289a044aaa6d1383aedf45
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 15:23:20 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -15,7 +15,7 @@ echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' >>
 STEP=$(echo "${FAILED_STEP}" | jq -r ".step") && echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
 NAME=$(echo "${FAILED_STEP}" | jq -r ".name") && echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' >> $BASH_ENV
 #echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' >> $BASH_ENV
-COMMITTER_NAME=$(echo $BUILD_INFO | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' && echo 'export COMMITTER_NAME=$(echo $BUILD_INFO | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' >> $BASH_ENV
+COMMITTER_NAME=$(echo $BUILD_INFO | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")') && echo 'export COMMITTER_NAME=$(echo $BUILD_INFO | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' >> $BASH_ENV
 echo $COMMITTER_NAME >> envars.txt
 #echo $CIRCLE_API_TOKEN  >> envars.txt
 #echo $ALLOCATION_ID  >> envars.txt
