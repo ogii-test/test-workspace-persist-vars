d2a7de7266a3ca36ca90ed2e4ff5358d9abe2b8c
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 14:58:15 +0900

    Update generate_vars.sh
    [skip ci]

--- generate_vars.sh
+++ generate_vars.sh
@@ -11,16 +11,16 @@ FAILED_STEP=$BUILD_INFO | jq '.steps | .[] | flatten | map(select(.status? == "f
 echo $BUILD_INFO
 echo $FAILED_STEP
 
-echo 'export THE_FAILED_STEP=$FAILED_STEP' >> $BASH_ENV
+#echo 'export THE_FAILED_STEP=$FAILED_STEP' >> $BASH_ENV
 echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' >> $BASH_ENV
 echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
 echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' >> $BASH_ENV
-echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' >> $BASH_ENV
+#echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' >> $BASH_ENV
 #echo 'export COMMITTER_NAME=$( curl "https://circleci.com/api/v1.1/project/${API_URL}?circle-token=${CIRCLE_API_TOKEN}" | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' >> $BASH_ENV
 #echo $COMMITTER_NAME >> envars.txt
 #echo $CIRCLE_API_TOKEN  >> envars.txt
 #echo $ALLOCATION_ID  >> envars.txt
-echo $FAILED_STEP  >> envars.txt
+#echo $FAILED_STEP  >> envars.txt
 echo $STEP  >> envars.txt
 echo $NAME  >> envars.txt
 echo $THE_FAILED_STEP  >> envars.txt
