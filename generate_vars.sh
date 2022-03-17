7297ca045aa47b81cb3e5af5aeb92dfe68c9649f
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 15:19:55 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -13,7 +13,7 @@ echo $FAILED_STEP
 
 #echo 'export THE_FAILED_STEP=$FAILED_STEP' >> $BASH_ENV
 echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' >> $BASH_ENV
-echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
+STEP=$(echo "${FAILED_STEP}" | jq -r ".step") && echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
 echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' >> $BASH_ENV
 #echo 'export API_URL=$(echo $CIRCLE_BUILD_URL | cut -d/ -f4-7)' >> $BASH_ENV
 #echo 'export COMMITTER_NAME=$( curl "https://circleci.com/api/v1.1/project/${API_URL}?circle-token=${CIRCLE_API_TOKEN}" | tail -n23 | grep committer_name | cut -f2- -d: | grep -o -P "(?<=\").*(?=\")")' >> $BASH_ENV
