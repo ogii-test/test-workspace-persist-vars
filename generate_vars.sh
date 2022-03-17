4d2ac46e2522c0e17a648ac210a484137118b34a
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 16:21:26 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -7,9 +7,12 @@ sleep 1s
 
 BUILD_INFO=$(curl -X GET -H "Circle-Token: $CIRCLECI_TOKEN" -H "Content-type: application/json" "https://circleci.com/api/v1.1/project/${API_URL}")
 FAILED_STEP=$(echo $BUILD_INFO | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')
-STEP=$(echo "${FAILED_STEP}" | jq -r ".step") && echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' >> $BASH_ENV
-NAME=$(echo "${FAILED_STEP}" | jq -r ".name") && echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' >> $BASH_ENV
-COMMITTER_NAME=$(echo "${BUILD_INFO}" | tail -n23 | grep committer_name | cut -f2- -d: | cut -d\" -f2) && echo 'export COMMITTER_NAME=$(echo "${BUILD_INFO}" | tail -n23 | grep committer_name | cut -f2- -d: | cut -d\" -f2)' >> $BASH_ENV
+STEP=$(echo "${FAILED_STEP}" | jq -r ".step")
+echo 'export STEP=$(echo "${FAILED_STEP}" | jq -r ".step")' | sudo tee -a $BASH_ENV
+NAME=$(echo "${FAILED_STEP}" | jq -r ".name")
+echo 'export NAME=$(echo "${FAILED_STEP}" | jq -r ".name")' | sudo tee -a $BASH_ENV
+COMMITTER_NAME=$(echo "${BUILD_INFO}" | tail -n23 | grep committer_name | cut -f2- -d: | cut -d\" -f2)
+echo 'export COMMITTER_NAME=$(echo "${BUILD_INFO}" | tail -n23 | grep committer_name | cut -f2- -d: | cut -d\" -f2)' | sudo tee -a $BASH_ENV
 
 echo $COMMITTER_NAME >> envars.txt
 echo $STEP  >> envars.txt
