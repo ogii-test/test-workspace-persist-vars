2e571e3d6f8d62a65dac56a112aca1898c34e8c1
Aaron Clark <aaron.clark@circleci.com>
2022-03-17 14:53:37 +0900

    Update generate_vars.sh

--- generate_vars.sh
+++ generate_vars.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #Script to generate vars to be used in Slack notifications
-sleep 5s
+
 API_URL="gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM"
-echo $API_URL
-FAILED_STEP=$(curl -X GET -H "Circle-Token: $CIRCLECI_TOKEN" -H "Content-type: application/json" "https://circleci.com/api/v1.1/project/${API_URL}" | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')
-echo $FAILED_STEP
+
+sleep 1s
+
+BUILD_INFO=$(curl -X GET -H "Circle-Token: $CIRCLECI_TOKEN" -H "Content-type: application/json" "https://circleci.com/api/v1.1/project/${API_URL}" | jq '.steps | .[] | flatten | .[]')
+#FAILED_STEP BUILD_INFO=$(curl -X GET -H "Circle-Token: $CIRCLECI_TOKEN" -H "Content-type: application/json" "https://circleci.com/api/v1.1/project/${API_URL}" | jq '.steps | .[] | flatten | map(select(.status? == "failed")) | .[] | {allocation_id, step, name}')
+echo $BUILD_INFO
 
 echo 'export THE_FAILED_STEP=$FAILED_STEP' >> $BASH_ENV
 echo 'export ALLOCATION_ID=$(echo "${FAILED_STEP}" | jq -r ".allocation_id")' >> $BASH_ENV
